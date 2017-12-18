c78f73a8f6867bcb750ade956471418f48f43c40
Emad <emadabel@gmail.com>
2017-12-18 19:03:38 +0200

    Replace normal AsyncTask with AsyncTaskLoader.

--- app/src/main/java/com/example/android/moviesdatabase/MainActivity.java
+++ app/src/main/java/com/example/android/moviesdatabase/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.android.moviesdatabase;
 
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,8 +23,12 @@ import com.example.android.moviesdatabase.utilities.NetworkUtils;
 import java.net.URL;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements MovieDbAdapter.MovieDbAdapterOnClickHandler {
+public class MainActivity extends AppCompatActivity implements
+        MovieDbAdapter.MovieDbAdapterOnClickHandler,
+        LoaderManager.LoaderCallbacks<ArrayList<DatasetUtils>> {
 
+    private static final String SEARCH_QUERY_EXTRA = "query";
+    private static final int MOVIES_LOADER_ID = 11;
     ArrayList<DatasetUtils> list;
     private EditText mMovieSearchEditText;
     private Button mSearchButton;
@@ -62,23 +69,36 @@ public class MainActivity extends AppCompatActivity implements MovieDbAdapter.Mo
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setAdapter(mMovieDbAdapter);
 
+        LoaderManager.LoaderCallbacks<ArrayList<DatasetUtils>> callback = MainActivity.this;
+
+        getSupportLoaderManager().initLoader(MOVIES_LOADER_ID, null, callback);
+
         mSearchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (mMovieSearchEditText.getText().toString().trim().isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Please type something to search", Toast.LENGTH_SHORT).show();
-                    return;
-                }
                 loadMovieData();
             }
         });
     }
 
     private void loadMovieData() {
-        showMovieDataView();
-
         String query = mMovieSearchEditText.getText().toString();
-        new FetchMovieTask().execute(query);
+
+        if (TextUtils.isEmpty(query)) {
+            Toast.makeText(MainActivity.this, "Please type something to search", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Bundle queryBundle = new Bundle();
+        queryBundle.putString(SEARCH_QUERY_EXTRA, query);
+
+        LoaderManager loaderManager = getSupportLoaderManager();
+        Loader<ArrayList<DatasetUtils>> moviesSearchLoader = loaderManager.getLoader(MOVIES_LOADER_ID);
+        if (moviesSearchLoader == null) {
+            loaderManager.initLoader(MOVIES_LOADER_ID, queryBundle, this);
+        } else {
+            loaderManager.restartLoader(MOVIES_LOADER_ID, queryBundle, this);
+        }
     }
 
     private void showMovieDataView() {
@@ -98,45 +118,69 @@ public class MainActivity extends AppCompatActivity implements MovieDbAdapter.Mo
         startActivity(intent);
     }
 
-    public class FetchMovieTask extends AsyncTask<String, Void, ArrayList<DatasetUtils>> {
+    @Override
+    public Loader<ArrayList<DatasetUtils>> onCreateLoader(int id, final Bundle args) {
+        return new AsyncTaskLoader<ArrayList<DatasetUtils>>(this) {
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            mLoadingIndicator.setVisibility(View.VISIBLE);
-        }
+            ArrayList<DatasetUtils> mMoviesData = null;
 
-        @Override
-        protected ArrayList<DatasetUtils> doInBackground(String... strings) {
+            @Override
+            protected void onStartLoading() {
+                if (args == null) {
+                    return;
+                }
 
-            if (strings.length == 0) {
-                return null;
+                if (mMoviesData != null) {
+                    deliverResult(mMoviesData);
+                } else {
+                    mLoadingIndicator.setVisibility(View.VISIBLE);
+                    forceLoad();
+                }
             }
 
-            String query = strings[0];
-            URL movieSearchUrl = NetworkUtils.buildUrl(MainActivity.this, query, NetworkUtils.SearchType.BY_SEARCH);
+            @Override
+            public ArrayList<DatasetUtils> loadInBackground() {
+                String query = args.getString(SEARCH_QUERY_EXTRA);
 
-            try {
-                String jsonOmdbResponse = NetworkUtils
-                        .getResponseFromHttpUrl(movieSearchUrl);
+                if (query == null || TextUtils.isEmpty(query)) {
+                    return null;
+                }
 
-                return JsonUtils.getOmdbDataFromJson(jsonOmdbResponse);
+                URL movieSearchUrl = NetworkUtils.buildUrl(MainActivity.this, query, NetworkUtils.SearchType.BY_SEARCH);
 
-            } catch (Exception e) {
-                e.printStackTrace();
-                return null;
+                try {
+                    String jsonOmdbResponse = NetworkUtils
+                            .getResponseFromHttpUrl(movieSearchUrl);
+
+                    return JsonUtils.getOmdbDataFromJson(jsonOmdbResponse);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return null;
+                }
             }
-        }
 
-        @Override
-        protected void onPostExecute(ArrayList<DatasetUtils> movieData) {
-            mLoadingIndicator.setVisibility(View.INVISIBLE);
-            if (movieData != null) {
-                showMovieDataView();
-                mMovieDbAdapter.setMovieData(movieData);
-            } else {
-                showErrorMessage();
+            @Override
+            public void deliverResult(ArrayList<DatasetUtils> moviesData) {
+                mMoviesData = moviesData;
+                super.deliverResult(moviesData);
             }
+        };
+    }
+
+    @Override
+    public void onLoadFinished(Loader<ArrayList<DatasetUtils>> loader, ArrayList<DatasetUtils> moviesData) {
+        mLoadingIndicator.setVisibility(View.INVISIBLE);
+
+        if (moviesData == null) {
+            showErrorMessage();
+        } else {
+            showMovieDataView();
+            mMovieDbAdapter.setMovieData(moviesData);
         }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<ArrayList<DatasetUtils>> loader) {
+
     }
 }
